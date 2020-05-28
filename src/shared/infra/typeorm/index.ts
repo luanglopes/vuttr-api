ef18310d1fb65c41bf17ebcc4203566bfc7a8fdc
Luan Lopes <luanglopes@gmail.com>
2020-05-28 02:33:42 -0300

    adjusts for porduction build

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,19 @@
-import { createConnection } from 'typeorm'
+import { createConnection, getConnectionOptions } from 'typeorm'
 
-createConnection()
+const initDatabase = async () => {
+  const connOptions = await getConnectionOptions()
+
+  if (process.env.NODE_ENV === 'production') {
+    Object.assign(connOptions, {
+      entities: [
+        connOptions.entities?.map((path) =>
+          path.toString().replace('src', 'dist'),
+        ),
+      ],
+    })
+  }
+
+  await createConnection(connOptions)
+}
+
+initDatabase()
